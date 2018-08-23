52c002669e4ae279041e376726b4900502b97df9
Sumit Saurabh <ssaurabh262@gmail.com>
2018-08-23 19:09:43 +0530

    hello world solidity

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-var Migrations = artifacts.require("./Migrations.sol");
+var helloworld = artifacts.require("./helloworld.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(helloworld);
 };
